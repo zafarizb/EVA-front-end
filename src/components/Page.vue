f8eb61753dc5d2902a8097544b13073ea98141c7
ggyhug <guogy6@mail2.sysu.edu.cn>
2021-03-09 14:16:55 +0800

    page new

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -5,18 +5,36 @@
       <el-button
         type="primary"
         style="float: right"
+        icon="el-icon-edit"
         @click="dialogFormVisible = true"
         >添加任务</el-button
       >
       <el-dialog title="任务信息" :visible.sync="dialogFormVisible">
-        <el-form :model="form">
-          <el-form-item label="任务名称" :label-width="formLabelWidth">
+        <el-form
+          :model="form"
+          ref="dataForm"
+          label-position="left"
+          label-width="90px"
+          style="width: 400px; margin-left: 50px"
+        >
+          <el-form-item label="任务名称" prop="name">
             <el-input v-model="form.name" autocomplete="off"></el-input>
           </el-form-item>
-          <el-form-item label="任务文件" :label-width="formLabelWidth">
-            <el-input v-model="form.file" autocomplete="off"></el-input>
+          <el-form-item label="任务文件" :prop="file">
+            <el-select
+              v-model="form.file"
+              class="filter-item"
+              placeholder="请选择文件"
+            >
+              <el-option
+                v-for="item in tableOption"
+                :lable="item.label"
+                :key="item.key"
+                :value="item.key"
+              />
+            </el-select>
           </el-form-item>
-          <el-form-item label="执行模型" :label-width="formLabelWidth">
+          <el-form-item label="执行模型" :prop="model">
             <el-select v-model="form.model" placeholder="请选择模型">
               <el-option label="模型一" value="ssd"></el-option>
               <el-option label="模型二" value="faster_rcnn"></el-option>
@@ -25,9 +43,7 @@
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogFormVisible = false"
-            >确 定</el-button
-          >
+          <el-button type="primary" @click="createPro()">确 定</el-button>
         </div>
       </el-dialog>
     </div>
@@ -50,7 +66,7 @@
               >查看详情</el-button
             >
             <el-button
-              @click.native.prevent="deleteRow(scope.$index, tableData)"
+              @click.native.prevent="deletePro(scope.$index)"
               type="text"
               size="small"
             >
@@ -69,8 +85,60 @@ export default {
     handleClick(index, row) {
       console.log(index, row);
     },
-    deleteRow(index, rows) {
-      rows.splice(index, 1);
+    deletePro(index) {
+      this.$confirm("确定删除这条任务吗？", {
+        confirmButtonText: "确定",
+        cancleButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          var name = this.tableData[index].name;
+          console.log(name);
+          this.axios
+            .post("//127.0.0.1:8000/image", this.$qs.stringify({ name: name }))
+            .then((res) => res.data)
+            .then((data) => {
+              console.log(data);
+              if (data.status == 0) {
+                this.tableData.splice(index, 1);
+                this.$message({
+                  type: "success",
+                  message: "删除成功",
+                });
+              }
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+    createPro() {
+      if (!this.form.name) {
+        this.$message.error("请输入任务名！");
+      } else if (!this.form.model) {
+        this.$message.error("请选择模型！");
+      } else if (!this.form.file) {
+        this.$message.error("请选择文件！");
+      } else {
+        axios
+          .post("//127.0.0.1:8000/image", {
+            name: this.form.name,
+            file: this.form.file,
+            model: this.form.model,
+          })
+          .then((res) => {
+            console.log(res.data);
+            if (res.data.status == 0) {
+              this.tableData = res.data.tableData;
+              alert("添加任务成功！");
+            } else {
+              alert("添加任务失败！");
+            }
+          });
+      }
     },
   },
   data() {
@@ -93,19 +161,18 @@ export default {
           zip: 200333,
         },
       ],
+      tableOption: [
+        { label: "C://user//image//page1", key: "图像1" },
+        { label: "C://user//image//page2", key: "图像2" },
+      ],
       dialogTableVisible: false,
       dialogFormVisible: false,
       form: {
         name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
+        model: "",
+        file: "",
       },
-      formLabelWidth: "120px",
+      formLabelWidth: "80px",
     };
   },
   created: function () {
@@ -114,13 +181,14 @@ export default {
       //创建接口
       methods: "get", //类型为get请求
       url: "//127.0.0.1:8000/image", //请求的接口地址
-      params:{
-        userid: sessionStorage.getItem('accessToken')
-      }
+      params: {
+        userid: sessionStorage.getItem("accessToken"),
+      },
     }).then(function (response) {
       //请求成功返回
-      _this.tableData = response.data.tableData; //数据打包，打包在data中创建的数组 我这里是focus数组
-      console.log(response.data)
+      _this.tableData = response.data.tableData; //数据打包，打包在data中创建的数组
+      _this.tableOption = response.data.tableOption;
+      console.log(response.data);
     });
   },
 };
--- src/main.js
+++ src/main.js
@@ -16,16 +16,17 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-router.beforeEach((to,from,next) => {
+/*router.beforeEach((to,from,next) => {
 	// 如果即将进入的路由对象是登录页，则进行跳转，否则验证是否携带accessToken,如果有，则进
 	// 行跳转，没有，则不允许跳转
-    if(to.path === "/login"){
-        next()
-    }else{
-        if (sessionStorage.getItem('accessToken')){
-            next()
-        } else {
-            next("/login")
-        }
-    }
+    //if(to.path === "/login"){
+        //next()
+    //}else{
+    //    if (sessionStorage.getItem('accessToken')){
+    //        next()
+    //    } else {
+    //        next("/login")
+    //    }
+    //}
 })
+*/
