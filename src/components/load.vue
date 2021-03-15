92c33a6a056d421e7e53598dc8dd61bf2b2cf0de
ggyhug <guogy6@mail2.sysu.edu.cn>
2021-03-15 16:35:21 +0800

    load

--- src/components/load.vue
+++ src/components/load.vue
@@ -1,26 +1,51 @@
 <template>
   <div>
+    <p style="float: left">用户文件管理列表</p>
     <div class="upload">
       <el-upload
         class="upload-demo"
-        action="https://jsonplaceholder.typicode.com/posts/"
-        accept="image/jpeg,image/png"
-        :on-preview="handlePreview"
-        :on-remove="handleRemove"
-        :before-upload="beforeUpload"
-        :before-remove="beforeRemove"
+        action="doupload()"
+        accept="image/jpeg,image/png,aplication/zip"
+        :before-upload="before_Upload"
+        ref="newupload"
+        :data="data"
         multiple
-        :limit="5"
-        :on-exceed="handleExceed"
         :file-list="fileList"
+        :show-file-list="false"
+        multiple
       >
         <el-button size="big" type="primary">点击上传</el-button>
+        <div slot="tip" class="el-upload__tip">
+          允许上传jpg/png图片和zip视频文件
+        </div>
       </el-upload>
+          </div>
+    <div class="form">
+      <el-table :data="tableData" height="600" border style="width: 100%">
+        <el-table-column prop="name" label="文件名称" width="250">
+        </el-table-column>
+        <el-table-column prop="model" label="执行模型" width="250">
+        </el-table-column>
+        <el-table-column prop="state" label="任务状态" width="250">
+        </el-table-column>
+        <el-table-column prop="date" label="创建时间" width="250">
+        </el-table-column>
+        <el-table-column prop="action" label="操作">
+          <template slot-scope="scope">
+            <el-button @click="download(scope.$index)" type="text" size="small"
+              >下载文件</el-button
+            >
+            <el-button
+              @click.native.prevent="deletePro(scope.$index)"
+              type="text"
+              size="small"
+            >
+              删除文件
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
-    <div class="download">
-      <el-button size="big" type="primary" @click="download_file"
-        >点击下载</el-button
-      >
     </div>
   </div>
 </template>
@@ -30,47 +55,127 @@ export default {
   data() {
     return {
       fileList: [],
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "ttt",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+      ],
     };
   },
   methods: {
-    handleRemove(file, fileList) {
-      console.log(file, fileList);
+    doupload() {
+      return "//127.0.0.1:8000/file/upload";
     },
-    handlePreview(file) {
-      console.log(file);
+    //上传
+    before_Upload(file) {
+      //  表单提交方式
+      let FormDatas = new FormData();
+      FormDatas.append("file", file);
+      console.log(FormDatas);
+      let that = this;
+      that
+        .$http({
+          method: "post",
+          url: "//127.0.0.1:8000/file/upload", //  二次接口
+          headers: headers("multipart/form-data"),
+          data: FormDatas,
+        })
+        .then(function (res) {
+          if (res.data.code == 1) {
+            that.$message.success(res.data.msg);
+          } else {
+            that.$message.error(res.data.msg);
+          }
+        });
     },
-    handleExceed(files, fileList) {
-      this.$message.warning(
-        `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${
-          files.length + fileList.length
-        } 个文件`
-      );
-    },
-    beforeUpload(file) {
-      const isIMAGE = file.type == "image/jpeg" || "image/gif" || "image/png";
-      const isLt1M = file.size / 1024 / 1024 < 1;
+    //下载
+    download(index) {
+      var name = this.tableData[index].name;
+      let that = this;
+      let formData = new FormData();
+      formData.append("filename", name);
+      console.log(formData);
+      this.$http({
+        url: "//127.0.0.1:8000/file/download",
+        method: "POST",
+        headers: headers("multipart/form-data"),
+        responseType: "blob",
+        data: formData,
+      })
+        .then(function (res) {
+          let result = res.data;
+          let url = window.URL.createObjectURL(new Blob([result])); //处理文档流
+          let link = document.createElement("a");
+          link.style.display = "none";
+          link.href = url;
 
-      if (!isIMAGE) {
-        this.$message.error("上传文件只能是图片格式!");
-      }
-      if (!isLt1M) {
-        this.$message.error("上传文件大小不能超过 1MB!");
-      }
-      return isIMAGE && isLt1M;
+          link.download = fileTye;
+          document.body.appendChild(link);
+          link.click();
+        })
+        .catch(function (error) {
+          that.$message.error(error);
+        });
     },
-    beforeRemove(file, fileList) {
-      return this.$confirm(`确定移除 ${file.name}?`);
+    deletePro(index) {
+      this.$confirm("确定删除该文件吗？", {
+        confirmButtonText: "确定",
+        cancleButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          var name = this.tableData[index].name;
+          console.log(name);
+          this.$axios
+            .post("//127.0.0.1:8000/file/delete", {
+              name: name,
+              userid: sessionStorage.getItem("accessToken"),
+            })
+            .then((res) => res.data)
+            .then((data) => {
+              console.log(data);
+              if (data.status == 1) {
+                this.tableData.splice(index, 1);
+                this.$message({
+                  type: "success",
+                  message: "删除成功",
+                });
+              }
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
     },
+  },
+  created: function () {
+    var _this = this;
+    this.$axios({
+      //创建接口
+      methods: "get", //类型为get请求
+      url: "//127.0.0.1:8000/file/info", //请求的接口地址
+      params: {
+        userid: sessionStorage.getItem("accessToken"),
+      },
+    }).then(function (response) {
+      //请求成功返回
+      _this.tableData = response.data.tableData; //数据打包，打包在data中创建的数组
+      console.log(response.data);
+    });
   },
 };
 </script>
 <style scoped>
 .upload {
-  padding-left: 0px;
-  padding-top: 100px;
-}
-.download {
-  padding-left: 0px;
-  padding-top: 200px;
+  padding-left: 1100px;
+  padding-top: 0px;
 }
 </style>
